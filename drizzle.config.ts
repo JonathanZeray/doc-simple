63769dc10e9193060b712ec12169cc46e72b4c6a
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-08-13 15:07:16 +0200

    Remove comment.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
-// Load environment variables from .env.local or .env
 dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
