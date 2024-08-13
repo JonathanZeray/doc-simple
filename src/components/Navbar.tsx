c2f31d097027ad8b8ca48cae3541cd6ff093ba57
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-08-13 11:20:19 +0200

    Created navbar and imported it to layout.tsx file.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+  return (
+    <nav className="p-4 pb-8">
+      <ul className="flex justify-between">
+        <li>home</li>
+        <li>login</li>
+        <li>get started</li>
+        <li>about</li>
+      </ul>
+    </nav>
+  );
+};
