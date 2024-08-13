c2f4cc5a8887fd86cc91da13225727eeec51bb2a
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-08-13 11:17:15 +0200

    Created empty landing page component to render on homepage. Imported imageAnalyzer to homepage aswell.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import ImageAnalyzer from "@/components/ImageAnalyzer";
+import { LandingPage } from "@/components/LandingPage";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="">
+      <LandingPage />
+      <ImageAnalyzer />
+    </main>
   );
 }
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,9 @@
+export const LandingPage = () => {
+  return (
+    <>
+      <div className="w-full h-screen">
+        <h1>This will be the landing page. Welcome!</h1>
+      </div>
+    </>
+  );
+};
