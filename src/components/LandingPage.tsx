d0897f9d8dcb798dee2c6e69969569a57163e387
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-08-14 09:40:26 +0200

    Added image to landing page.

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 export const LandingPage = () => {
   return (
     <>
-      <div className="w-full h-screen">
+      <div className="flex flex-col-reverse sm:flex-row w-full h-screen gap-16 sm:gap-0">
         <h1>This will be the landing page. Welcome!</h1>
+        <Image
+          src="/images/form-illustration.webp"
+          width={500}
+          height={500}
+          alt="Form illustration"
+          className="sm:shadow-lg rounded-lg overflow-hidden"
+        />
       </div>
     </>
   );
